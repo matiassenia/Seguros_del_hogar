b1cdb1a793bad6872cdd7f12eda8b380e31b4dee
Matías Senia <matiassenia9@gmail.com>
2024-02-13 15:14:16 -0300

    REFACTOR(ALL)Se agragan funciones y nuevas variables

--- cotizador-hogar/js/app.js
+++ cotizador-hogar/js/app.js
@@ -1,3 +1,7 @@
+const selectPropiedad = document.querySelector("select#propiedad")
+const selectUbicacion = document.querySelector("select#ubicacion")
+const inputMetros2 = document.querySelector("input#metros2")
+const btnCotizar = document.querySelector("button.button.button-outline")
 
 
 // Definición de las variables de factor multiplicador (fm) para el tipo de vivienda y ubicación.
@@ -13,33 +17,58 @@ let metrosCuadrados = prompt('Ingresa los metros cuadrados de la vivienda: ');
 // Solicitar al usuario que ingrese un tipo de vivienda a través de un cuadro de diálogo prompt()
 let tipoVivienda = prompt('Seleccione su tipo de vivienda: \n' +  "('Casa', 'P.H.', 'Dto. Edificio', 'Barrio Privado', 'Oficina', 'Local comercial', 'Depósito logística')")
 
-// Validar si el tipo de vivienda ingresado es válido y asignar el factor multiplicador correspondiente.
-if (tipoVivienda !== '') {
+
+function cargarComboPropiedad() {
+    let opcionPropiedad
     for (let propiedad of datosPropiedad) {
-        if (propiedad.tipo === tipoVivienda) {
-            fmPropiedad = propiedad;
-            break
+        opcionPropiedad += "<option>" + propiedad.tipo + "</option>"
+    }
+    selectPropiedad.innerHTML = opcionPropiedad
+}
+
+function cargarComboUbicacion() {
+    let opcionUbicacion
+    for (let ubicacion of datosUbicacion) {
+        opcionUbicacion += "<option>" + ubicacion.tipo + "</option>"
+    }
+    selectUbicacion.innerHTML = opcionUbicacion
+}
+
+
+// Validar si el tipo de vivienda ingresado es válido y asignar el factor multiplicador correspondiente.
+function fmPropiedad() {
+    if (tipoVivienda.value !== '') {
+        for (let propiedad of datosPropiedad) {
+            if (propiedad.tipo === tipoVivienda.value) {
+                return propiedad;
+            }
         }
     }
 }
-
 // Solicitar al usuario que ingrese la ubicación de la vivienda.
 let tipoUbicacion = prompt ("Selecciona la ubicación de la vivienda: \n" + "('CABA', 'Tandil', 'Costa Atlantica', 'Patagonia Argentina')")
 
 // Validar si la ubicación ingresada es válida y asignar el factor multiplicador correspondiente.
-if (tipoUbicacion !== '') {
-    for (let ubicacion of datosUbicacion) {
-        if (ubicacion.tipo === tipoUbicacion) {
-            fmUbicacion = ubicacion;
-            break
+function fmUbicacion() {
+    if (tipoUbicacion.value !== '') {
+        for (let ubicacion of datosUbicacion) {
+            if (ubicacion.tipo === tipoUbicacion.value) {
+                return ubicacion;
+            }
         }
-    }
-} 
-
-// Verifica si todas las variables necesarias están definidas y si metros2 es un número.
-if (fmPropiedad && fmUbicacion && !isNaN(parseFloat(metrosCuadrados))){
-    let resultado = fmPropiedad.factor * fmUbicacion.factor * metrosCuadrados * costoBaseM2;
-    console.log("Resultado de la Póliza: $ " + resultado.toFixed(2));
-} else {
-    console.warn("Hubo un error en los datos ingresados.")
+    } 
 }
+// Verifica si todas las variables necesarias están definidas y si metros2 es un número.
+btnCotizar.onclick = function () {
+
+    if (fmPropiedad && fmUbicacion && !isNaN(parseFloat(metrosCuadrados))){
+        let resultado = fmPropiedad.factor * fmUbicacion.factor * metrosCuadrados * costoBaseM2;
+        console.log("Resultado de la Póliza: $ " + resultado.toFixed(2));
+    } else {
+        console.warn("Hubo un error en los datos ingresados.")
+    }
+
+}
+
+cargarComboPropiedad()
+cargarComboUbicacion() 
